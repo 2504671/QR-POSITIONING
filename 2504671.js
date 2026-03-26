393a11b1b582a984fbefea26fe2440b44181cd2f
Swastika Gautam <gautamswastika2000@gmail.com>
2026-03-26 23:03:31 +0200

    Update 2504671.js

--- 2504671.js
+++ 2504671.js
@@ -27,6 +27,22 @@ function startScanner() {
         console.error(err);
     });
 }
+try {
+    const data= JSON.parse(text);
+    // Move marker
+    if(data.top && data.left) {
+        showMarkerAt(data.top, data.left);
+    }
+    // Display inventory info
+    document.getElementById("name").innerText = "Name: " + data.name;
+    document.getElementById("store").innerText = "Store: " + (data.inStock ?? data.in_store);
+    document.getElementById("price").innerText = "price: " + data.price + " £";
+
+    //stop scanner
+    toggleScanner();
+} catch (e) {
+    console.error("Invalid QR code JSON", e);
+}
 
 function stopScanner() {
     reader.stop();
